e498309e53c4b6d718b24a3b96563e6326149378
Jim Basilio <jim.basilio@gmail.com>
2015-11-12 21:38:27 -0500

    refactoring to remove appcache. current code doesn't cache bust properly for bower code but cache busts for all hiit code so for now good enough

--- src/main/resources/public/static/angular_components/js/controllers/MainController.js
+++ src/main/resources/public/static/angular_components/js/controllers/MainController.js
@@ -2,32 +2,4 @@ hiit.controller("MainController", [ "$scope", "$auth", function($scope, $auth) {
 	$scope.authenticate = function(provider) {
 		$auth.authenticate(provider);
 	};
-
-	
-	// below here is app cache logic
-	
-	var worker = new Worker('static/angular_components/js/webworkers/appCacheWorker.js');
-	var appCache = window.applicationCache;
-
-	var onError = function(e) {
-		console.log("Error: line ", e.lineno, " in ", e.filename, ": ", e.message);
-	};
-	
-	appCache.addEventListener('updateready', function(e) {
-	      // Browser downloaded a new app cache.
-	      if (confirm('A new version of this site is available. Load it?')) {
-	        window.location.reload(true);
-	      }
-	  }, false);
-	
-	worker.onmessage = function() {
-		appCache.update(); // check for update, event will fire if ready
-
-		worker.postMessage('back at you');
-	};
-
-	setTimeout(function() {
-		worker.postMessage('start it off'); // Send data to our worker.
-	}, 10000);
-
 } ]);
--- src/main/resources/public/static/angular_components/js/webworkers/appCacheWorker.js
+++
@@ -1,5 +0,0 @@
-self.onmessage = function(window_reference) {
-	setTimeout(function() {
-		self.postMessage('back at you');
-	}, 10000);
-};
